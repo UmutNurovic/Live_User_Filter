42128675570f32e01e52b2aecc705813c50f7ec3
Umut can Aktürk <akturkumutcan58@gmail.com>
2021-09-20 00:49:32 +0300

    UserFilter

---
+++ controller/index.js
@@ -0,0 +1,40 @@
+const result = document.getElementById('result');
+const filter = document.getElementById('filter');
+
+const listItems = [];
+console.log(listItems);
+getData();
+filter.addEventListener('input',(e)=>{
+    filterData(e.target.value);
+});
+function getData (){
+    fetch('https://randomuser.me/api/?results=50')
+    .then(res=> res.json())
+    .then(data=>{
+        
+        result.innerHTML = '';
+        data.results.forEach(user => {
+            const li  = document.createElement('li');
+            // store for filter 
+            listItems.push(li);
+            li.innerHTML = `
+            <img src="${user.picture.large}" alt="${user.name.first}">
+            <div class ="user-info">
+            <h4>${user.name.last}<h4/>
+            <p>${user.location.city}, ${user.location.country}<p/>
+            <div/>
+            `;
+            result.appendChild(li);
+        });
+    });
+}
+
+function filterData (searchTerm){
+    listItems.forEach(item=>{
+        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())){
+            item.classList.remove('hide');
+        }else{
+            item.classList.add('hide');
+        }
+    })
+}
